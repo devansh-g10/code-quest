45ae4f5e1461c15cb8fb9619abcfaba438502988
devansh-ce1 <devanshmaheshwari3011@gmail.com>
2026-03-20 23:52:40 +0530

    fix: increased backend body limit to 10MB for image uploads

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ import Question from './models/Question.js';
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
